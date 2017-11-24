8fdef4dae5be540222ee66d61c57b57212c48a26
Amael Béghin <abeghin@gmail.com>
2017-11-24 18:10:03 +0100

    test seed heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,25 @@
 # Ingredient.create(name: "mint leaves")
 
 
-require 'json'
-require 'open-uri'
+# require 'json'
+# require 'open-uri'
 
 Cocktail.destroy_all
 
-url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json"
-cocktails_serialized = open(url).read
-cocktails = JSON.parse(cocktails_serialized)
-cocktails.first(12).each do |cocktail|
-Cocktail.create(name: cocktail['name'])
-end
+# url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json"
+# cocktails_serialized = open(url).read
+# cocktails = JSON.parse(cocktails_serialized)
+# cocktails.first(12).each do |cocktail|
+# Cocktail.create(name: cocktail['name'])
+# end
 
 Ingredient.destroy_all
 
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
+# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
 
-ingredients["drinks"].each do |ingredient|
-Ingredient.create(name: ingredient['strIngredient1'])
-end
+# ingredients["drinks"].each do |ingredient|
+#   Ingredient.create(name: ingredient['strIngredient1'])
+# end
 
