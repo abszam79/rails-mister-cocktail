5d5f97b81fa87136cbb927331e31cd3e7b0beab9
Amael Béghin <abeghin@gmail.com>
2017-11-24 12:25:03 +0100

    Add figaro - Protect my secret data in config/application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,17 +18,17 @@ Cocktail.destroy_all
 url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json"
 cocktails_serialized = open(url).read
 cocktails = JSON.parse(cocktails_serialized)
-cocktails.first(10).each do |cocktail|
+cocktails.first(12).each do |cocktail|
 Cocktail.create(name: cocktail['name'])
 end
 
-# Ingredient.destroy_all
+Ingredient.destroy_all
 
-# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# ingredients_serialized = open(url).read
-# ingredients = JSON.parse(ingredients_serialized)
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
 
-# ingredients["drinks"].each do |ingredient|
-# Ingredient.create(name: ingredient['strIngredient1'])
-# end
+ingredients["drinks"].each do |ingredient|
+Ingredient.create(name: ingredient['strIngredient1'])
+end
 
