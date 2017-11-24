d259186c3b86f96599adbfd5f94dd730a3f80186
Amael Béghin <abeghin@gmail.com>
2017-11-24 11:49:51 +0100

    heroku prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,17 @@
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
+
+url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json"
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+cocktails.first(10).each do |cocktail|
+Cocktail.create(name: cocktail['name'])
+end
+
+
+
 url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
